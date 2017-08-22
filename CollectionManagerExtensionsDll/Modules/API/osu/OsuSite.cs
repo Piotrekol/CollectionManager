ac3550d671122a6c19be5aba1ba6e8ecc3bb9966
Piotrekol <nara1011@gmail.com>
2017-08-22 16:02:09 +0200

    Add logging capability to OsuSite

--- CollectionManagerExtensionsDll/Modules/API/osu/OsuSite.cs
+++ CollectionManagerExtensionsDll/Modules/API/osu/OsuSite.cs
@@ -10,19 +10,26 @@ namespace CollectionManagerExtensionsDll.Modules.API.osu
 {
     public class OsuSite
     {
+        private static readonly string parsingPage = "Getting page {0} of {1}";
+        private static readonly string parsingComplete = "Finished Processing of {0} pages";
+
         private static readonly string _baseUrl = "https://osu.ppy.sh/p";
         private static readonly string UserPpRankingUrl = _baseUrl + "/pp/?m=0&s=3&o=1&f=0&page={0}";
         private static readonly int UsersPerPage = 50;
+        public delegate void LogUsernameGeneration(string logMessage, int completionPrecentage);
 
-        public List<string> GetUsernames(int startRank, int endRank)
+        private LogUsernameGeneration _logger;
+        public List<string> GetUsernames(int startRank, int endRank, LogUsernameGeneration logger)
         {
             if (startRank < 0 || startRank > 10000 || endRank < 0 || endRank > 10000)
                 throw new ArgumentException("Parameters were not in allowed range(0-10000)");
+            _logger = logger;
 
             int startPage = Convert.ToInt32(Math.Floor((double)((double)startRank / (double)UsersPerPage))) + 1;
             int endPage = Convert.ToInt32(Math.Ceiling((double)((double)endRank / (double)UsersPerPage)));
 
-            int startUserIndex = (startRank % UsersPerPage)-1;
+            int startUserIndex = (startRank % UsersPerPage) - 1;
+            startUserIndex = startUserIndex < 0 ? 0 : startUserIndex;
             int endUserIndex = (endRank - 1) % UsersPerPage;
 
             return GetUsernames(startPage, endPage, startUserIndex, endUserIndex);
@@ -30,15 +37,19 @@ namespace CollectionManagerExtensionsDll.Modules.API.osu
 
         private List<string> GetUsernames(int startPage, int endPage, int startUserIndex, int endUserIndex)
         {
+            int processedPages = 0;
+            int totalPages = endPage - (startPage - 1);
             var usernames = new List<string>();
             using (var client = new WebClient())
             {
                 for (int i = startPage; i <= endPage; i++)
                 {
+                    _logger?.Invoke(string.Format(parsingPage, i, endPage), Convert.ToInt32(((float)processedPages++ / (float)totalPages) * 100));
                     var pageContents = client.DownloadString(string.Format(UserPpRankingUrl, i));
-                    var pageUsernames = GetUsernamesFromPage(pageContents, (i == startPage) ? startUserIndex : 0, (i == endPage) ? endUserIndex : UsersPerPage-1);
+                    var pageUsernames = GetUsernamesFromPage(pageContents, (i == startPage) ? startUserIndex : 0, (i == endPage) ? endUserIndex : UsersPerPage - 1);
                     usernames.AddRange(pageUsernames);
                 }
+                _logger?.Invoke(string.Format(parsingComplete, totalPages), Convert.ToInt32(((float)processedPages / (float)totalPages) * 100));
             }
             return usernames;
         }
--- CollectionManagerExtensionsDllTests/Modules/API/osu/OsuSiteTests.cs
+++ CollectionManagerExtensionsDllTests/Modules/API/osu/OsuSiteTests.cs
@@ -14,36 +14,43 @@ namespace CollectionManagerExtensionsDll.Modules.API.osu.Tests
         public void GetUsernamesFirstPage()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(1, 50);
+            var output = obj.GetUsernames(1, 50, null);
             Assert.IsTrue(output.Count == 50);
+        }
+        [TestMethod()]
+        public void GetUsernames1()
+        {
+            var obj = new OsuSite();
+            var output = obj.GetUsernames(300, 800, null);
+            Assert.IsTrue(output.Count == 500);
         }
 
         [TestMethod()]
         public void GetUsernames2Pages()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(1, 100);
+            var output = obj.GetUsernames(1, 100, null);
             Assert.IsTrue(output.Count == 100);
         }
         [TestMethod()]
         public void GetUsernamesSingleUser1()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(51, 51);
+            var output = obj.GetUsernames(51, 51, null);
             Assert.IsTrue(output.Count == 1);
         }
         [TestMethod()]
         public void GetUsernamesSingleUser2()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(1, 1);
+            var output = obj.GetUsernames(1, 1, null);
             Assert.IsTrue(output.Count == 1);
         }
         [TestMethod()]
         public void GetUsernamesFullFirstPartialSecondPage()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(1, 67);
+            var output = obj.GetUsernames(1, 67, null);
             Assert.IsTrue(output.Count == 67);
         }
         [TestMethod()]
@@ -51,14 +58,14 @@ namespace CollectionManagerExtensionsDll.Modules.API.osu.Tests
         public void GetUsernamesRankBoundary1()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(-1, 50);
+            var output = obj.GetUsernames(-1, 50, null);
         }
         [TestMethod()]
         [ExpectedException(typeof(ArgumentException))]
         public void GetUsernamesRankBoundary2()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(9951, 10001);
+            var output = obj.GetUsernames(9951, 10001, null);
         }
     }
 }
