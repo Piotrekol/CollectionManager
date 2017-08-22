78ca64dec02f1f30b847a955e72f554ae7a98528
Piotrekol <nara1011@gmail.com>
2017-08-22 16:00:17 +0200

    Implement UsernameGeneratorForm

---
+++ App/Interfaces/Controls/IUsernameGeneratorModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App.Interfaces
+{
+    public interface IUsernameGeneratorModel
+    {
+
+        event EventHandler Start;
+        event EventHandler Abort;
+        event EventHandler StatusChanged;
+        event EventHandler Complete;
+        List<string> GeneratedUsernames { get; set; }
+        string GeneratedUsernamesStr { get; }
+        string Status { set; get; }
+        int CompletionPrecentage { get; set; }
+        int StartRank { get; set; }
+        int EndRank { get; set; }
+        void EmitStart();
+        void EmitAbort();
+        void EmitComplete();
+
+    }
+}
