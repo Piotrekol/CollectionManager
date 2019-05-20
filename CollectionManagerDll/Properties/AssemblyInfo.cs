9f695f972ae03deda90586f753fd6c6a4bd8f68a
Piotrekol <nara1011@gmail.com>
2019-05-20 19:59:02 +0200

    Misc: modernize project & bump .net versions

--- CollectionManagerDll/Properties/AssemblyInfo.cs
+++ CollectionManagerDll/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CollectionManager")]
-[assembly: AssemblyDescription("Implements read/write access to db/osdb files, along with ability to edit these in a reasonable manner")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CollectionManager")]
-[assembly: AssemblyCopyright("Copyright Piotrekol©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -20,16 +8,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("533ab47a-d1b5-45db-a37e-f053fa3699c4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- CollectionManagerExtensionsDll/Modules/CollectionListGenerator/ListTypes/UserListGenerator.cs
+++ CollectionManagerExtensionsDll/Modules/CollectionListGenerator/ListTypes/UserListGenerator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using CollectionManager.DataTypes;
-using Interpolate;
+using InterpolateLibrary;
 
 namespace CollectionManagerExtensionsDll.Modules.CollectionListGenerator.ListTypes
 {
