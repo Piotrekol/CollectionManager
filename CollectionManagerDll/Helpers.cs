a2997eff5eb75498fb9fd72444b2d3093e5923b7
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2021-01-18 19:39:52 +0100

    Fix: Different beatmap hashes between runs on newer .NET versions

--- CollectionManagerDll/Helpers.cs
+++ CollectionManagerDll/Helpers.cs
@@ -1,4 +1,4 @@
-﻿using CollectionManager.DataTypes;
+using CollectionManager.DataTypes;
 using CollectionManager.Interfaces;
 using CollectionManager.Modules.FileIO;
 using CollectionManager.Modules.FileIO.OsuDb;
@@ -55,6 +55,25 @@ namespace CollectionManager
         public static Beatmap GetMap(this IReplay replay, IMapDataManager mapCacher)
         {
             return mapCacher.GetByHash(replay.MapHash);
+        }
+
+        public static int GetDeterministicHashCode(this string str)
+        {
+            unchecked
+            {
+                int hash1 = (5381 << 16) + 5381;
+                int hash2 = hash1;
+
+                for (int i = 0; i < str.Length; i += 2)
+                {
+                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                    if (i == str.Length - 1)
+                        break;
+                    hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+                }
+
+                return hash1 + (hash2 * 1566083941);
+            }
         }
     }
 }
