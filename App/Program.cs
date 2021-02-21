766aab7dc140b5bf581230fdae41c79bb9230279
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2021-02-21 22:02:58 +0100

    Add: Quick&dirty error handling
    #28

--- App/Program.cs
+++ App/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using App.Misc;
 
 namespace App
 {
@@ -13,9 +15,25 @@ namespace App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            AppDomain.CurrentDomain.UnhandledException += (_, exArgs) =>
+            {
+                if (exArgs.ExceptionObject is Exception exception)
+                    HandleException(exception);
+            };
+            Application.ThreadException += (_, exArgs) => HandleException(exArgs.Exception);
+            TaskScheduler.UnobservedTaskException += (_, exArgs) => HandleException(exArgs.Exception);
+
             var app = new Initalizer();
             app.Run(args);
             Application.Run(app);
+        }
+
+        public static void HandleException(Exception ex)
+        {
+            Helpers.SetClipboardText(ex.ToString());
+            MessageBox.Show(
+                $"There was a problem with CollectionManager and it needs to exit, exception text was copied to your clipboard:{Environment.NewLine}{ex}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
