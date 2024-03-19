fe5115e9bc511722b9aec81c570d9888ffc07f06
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2024-03-19 16:04:24 +0100

    Misc: Don't fail builds on 429 osu responses

--- CollectionManagerExtensionsDllTests/Modules/API/osu/OsuSiteTests.cs
+++ CollectionManagerExtensionsDllTests/Modules/API/osu/OsuSiteTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Net;
 
 namespace CollectionManagerExtensionsDll.Modules.API.osu.Tests
 {
@@ -10,14 +11,14 @@ namespace CollectionManagerExtensionsDll.Modules.API.osu.Tests
         public void GetUsernamesFirstPage()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(1, 50, null);
+            var output = Run(() => obj.GetUsernames(1, 50, null));
             Assert.IsTrue(output.Count == 50);
         }
         [TestMethod()]
         public void GetUsernames1()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(300, 800, null);
+            var output = Run(() => obj.GetUsernames(300, 800, null));
             Assert.IsTrue(output.Count == 500);
         }
 
@@ -25,28 +26,28 @@ namespace CollectionManagerExtensionsDll.Modules.API.osu.Tests
         public void GetUsernames2Pages()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(1, 100, null);
+            var output = Run(() => obj.GetUsernames(1, 100, null));
             Assert.IsTrue(output.Count == 100);
         }
         [TestMethod()]
         public void GetUsernamesSingleUser1()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(51, 51, null);
+            var output = Run(() => obj.GetUsernames(51, 51, null));
             Assert.IsTrue(output.Count == 1);
         }
         [TestMethod()]
         public void GetUsernamesSingleUser2()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(1, 1, null);
+            var output = Run(() => obj.GetUsernames(1, 1, null));
             Assert.IsTrue(output.Count == 1);
         }
         [TestMethod()]
         public void GetUsernamesFullFirstPartialSecondPage()
         {
             var obj = new OsuSite();
-            var output = obj.GetUsernames(1, 67, null);
+            var output = Run(() => obj.GetUsernames(1, 67, null));
             Assert.IsTrue(output.Count == 67);
         }
         [TestMethod()]
@@ -62,6 +63,24 @@ namespace CollectionManagerExtensionsDll.Modules.API.osu.Tests
         {
             var obj = new OsuSite();
             var output = obj.GetUsernames(9951, 10001, null);
+        }
+
+        private static T Run<T>(Func<T> func)
+            => RunFlaky<WebException, T>(func);
+
+        private static T RunFlaky<TEx, T>(Func<T> func)
+            where TEx : Exception
+        {
+            try
+            {
+                return func();
+            }
+            catch (TEx ex)
+            {
+                Assert.Inconclusive(ex.ToString());
+            }
+
+            return default;
         }
     }
 }
