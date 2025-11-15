bfb838128c70064e75075c3077031b6449cde1a5
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2025-11-15 12:41:00 +0100

    Misc: Replace reflection form shenanigans with DI

---
+++ CollectionManager.WinForms/FormServices.cs
@@ -0,0 +1,37 @@
+﻿namespace CollectionManager.WinForms;
+
+using CollectionManager.Common.Interfaces;
+using CollectionManager.Common.Interfaces.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+public sealed class FormServices
+{
+    public static void RegisterServices(ServiceCollection serviceCollection)
+    {
+        _ = serviceCollection.AddSingleton<IUserDialogs, UserDialogs>();
+
+        Dictionary<Type, IEnumerable<Type>> formTypes = typeof(FormServices)
+            .Assembly
+            .GetTypes()
+            .Where(x =>
+                !x.IsAbstract
+                && x.IsClass
+                && x.GetInterface(nameof(IForm)) == typeof(IForm))
+            .ToDictionary(
+                formType => formType,
+                (formType) => formType
+                    .GetInterfaces()
+                    .Where(interfaceType => interfaceType.Namespace.StartsWith(nameof(CollectionManager), StringComparison.InvariantCulture))
+                    .Except([typeof(IForm)]));
+
+        foreach ((Type formType, IEnumerable<Type> interfaces) in formTypes)
+        {
+            foreach (Type interfaceType in interfaces)
+            {
+                _ = serviceCollection.AddTransient(interfaceType, formType);
+            }
+        }
+    }
+}
