a99bc9ea941022525378f1643363174570cb328c
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2026-01-18 16:25:21 +0100

    Misc: Scuffed dark mode

--- CollectionManager.App.WinForms/Program.cs
+++ CollectionManager.App.WinForms/Program.cs
@@ -17,7 +17,9 @@ internal static class Program
     {
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
-
+#pragma warning disable WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        Application.SetColorMode(SystemColorMode.Dark);
+#pragma warning restore WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
         AppDomain.CurrentDomain.UnhandledException += (_, exArgs) =>
         {
             if (exArgs.ExceptionObject is Exception exception)
